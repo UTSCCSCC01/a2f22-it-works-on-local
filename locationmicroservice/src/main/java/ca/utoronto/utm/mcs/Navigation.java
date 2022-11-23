54ea26428bdc43e1d98388eab29ec9311927e7a1
mojohosseinii <hosseini.sayedmojtaba@gmail.com>
2022-11-23 18:02:30 -0500

    [locationmicroservice-getNearbyDriver] - Fixed minor bugs

--- locationmicroservice/src/main/java/ca/utoronto/utm/mcs/Navigation.java
+++ locationmicroservice/src/main/java/ca/utoronto/utm/mcs/Navigation.java
@@ -1,11 +1,16 @@
 package ca.utoronto.utm.mcs;
 
 import java.io.IOException;
+import java.io.OutputStream;
+import java.util.ArrayList;
+
 import org.json.*;
 import com.sun.net.httpserver.HttpExchange;
+import org.neo4j.driver.Record;
+import org.neo4j.driver.Result;
 
 public class Navigation extends Endpoint {
-    
+
     /**
      * GET /location/navigation/:driverUid?passengerUid=:passengerUid
      * @param driverUid, passengerUid
@@ -16,6 +21,65 @@ public class Navigation extends Endpoint {
 
     @Override
     public void handleGet(HttpExchange r) throws IOException, JSONException {
-        // TODO
+        String[] params = r.getRequestURI().toString().split("/");
+        if (params.length != 4 || params[2].isEmpty() || params[3].isEmpty())  {
+            this.sendStatus(r, 400);
+            return;
+        }
+
+        try {
+            JSONObject response = new JSONObject();
+            String parameters = params[3];
+            String[] inputs = parameters.split("\\?");
+            String driverUid = inputs[0];
+            String passengerUid = inputs[1].split("=")[1];
+
+            if(!this.dao.validUser(driverUid) || !this.dao.validUser(passengerUid)){
+                this.sendStatus(r, 400);
+                return;
+            } else {
+                Result result = this.dao.usersStreet(driverUid, passengerUid);
+                if (result.hasNext()) {
+                    try {
+                        Record user = result.next();
+                        String d_street = user.get("driver.street").asString();
+                        String p_street = user.get("passenger.street").asString();
+                        if(!this.dao.validRoad(d_street) || !this.dao.validRoad(p_street)){
+                            this.sendStatus(r, 400);
+                            return;
+                        } else {
+                            JSONObject navData = new JSONObject();
+                            ArrayList<JSONObject> route = this.dao.getPath(d_street, p_street);
+                            navData.put("total_time", this.dao.getTotalTime(d_street, p_street));
+                            navData.put("route", new JSONArray(route));
+                            response.put("data", navData);
+                            response.put("status", "OK");
+                        }
+                    } catch (Exception e) {
+                        this.sendStatus(r, 500);
+                        return;
+                    }
+                } else {
+                    this.sendStatus(r, 404);
+                    return;
+                }
+            }
+            byte[] val = response.toString().replace("\\\"", "").getBytes(); //Converts JSON Object to String
+            if (val == null) {
+                r.sendResponseHeaders(404, -1);
+                this.sendStatus(r, 404);
+                return;
+            } else {
+                r.sendResponseHeaders(200, val.length);
+            }
+            OutputStream os = r.getResponseBody();
+            os.write(val);
+            os.close();
+            return;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            this.sendStatus(r, 500);
+        }
     }
 }
