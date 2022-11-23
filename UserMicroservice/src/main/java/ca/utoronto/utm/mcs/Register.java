ad3bc4e20c9c629ef3785058550f8ee26e02ffcf
mojohosseinii <hosseini.sayedmojtaba@gmail.com>
2022-11-23 16:58:04 -0500

    [UserMicroservice-Register] - Fixed minor Bugs

--- UserMicroservice/src/main/java/ca/utoronto/utm/mcs/Register.java
+++ UserMicroservice/src/main/java/ca/utoronto/utm/mcs/Register.java
@@ -5,9 +5,6 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.IOException;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Random;
 
@@ -77,7 +74,7 @@ public class Register extends Endpoint {
                 uid = rand. nextInt(9000000) + 1000000;
                 resultHasNext = this.dao.getUsersFromUid(uid).next();
             }
-            String generatedPassword = hashingMD5(password);
+            String generatedPassword = this.dao.hashingMD5(password);
 
             this.dao.addUser(uid, email, generatedPassword, name, 0, false);
         }
@@ -89,31 +86,5 @@ public class Register extends Endpoint {
 
         // return 200 if everything is updated without error
         this.sendStatus(r, 200);
-    }
-    public String hashingMD5(String password) {
-        String generatedPassword = null;
-        try
-        {
-            // Create MessageDigest instance for MD5
-            MessageDigest md = MessageDigest.getInstance("MD5");
-
-            // Add password bytes to digest
-            md.update(password.getBytes());
-
-            // Get the hash's bytes
-            byte[] bytes = md.digest();
-
-            // These bytes[] has bytes in decimal format. Convert it to hexadecimal format
-            StringBuilder sb = new StringBuilder();
-            for (int i = 0; i < bytes.length; i++) {
-                sb.append(Integer.toString((bytes[i] & 0xff) + 0x100, 16).substring(1));
-            }
-
-            // Get complete hashed password in hex format
-            generatedPassword = sb.toString();
-        } catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
-        }
-        return generatedPassword;
     }
 }
