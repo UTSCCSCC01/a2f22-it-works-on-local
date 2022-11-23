7a8b3aa569fd4242a8ad1fafcf289b2cba523169
mojohosseinii <hosseini.sayedmojtaba@gmail.com>
2022-11-23 16:56:10 -0500

    [UserMicroservice-Register] - hashed the password  while registering a user

--- UserMicroservice/src/main/java/ca/utoronto/utm/mcs/Register.java
+++ UserMicroservice/src/main/java/ca/utoronto/utm/mcs/Register.java
@@ -5,6 +5,8 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.IOException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Random;
@@ -60,9 +62,7 @@ public class Register extends Endpoint {
 
         // if all the variables are still null then there's no variables in request so retrun 400
         if (email == null || name == null || password == null) {
-            JSONObject res = new JSONObject();
-            res.put("status", "Bad request body");
-            this.sendResponse(r, res, 200);
+            this.sendStatus(r, 200);
             return;
         }
 
@@ -77,20 +77,43 @@ public class Register extends Endpoint {
                 uid = rand. nextInt(9000000) + 1000000;
                 resultHasNext = this.dao.getUsersFromUid(uid).next();
             }
+            String generatedPassword = hashingMD5(password);
 
-            this.dao.addUser(uid, email, password, name, 0, false);
+            this.dao.addUser(uid, email, generatedPassword, name, 0, false);
         }
         catch (SQLException e) {
             e.printStackTrace();
-            JSONObject res = new JSONObject();
-            res.put("status", e.getMessage());
-            this.sendResponse(r, res, 500);
+            this.sendStatus(r, 500);
             return;
         }
 
         // return 200 if everything is updated without error
-        JSONObject res = new JSONObject();
-        res.put("status", "OK");
-        this.sendResponse(r, res, 200);
+        this.sendStatus(r, 200);
+    }
+    public String hashingMD5(String password) {
+        String generatedPassword = null;
+        try
+        {
+            // Create MessageDigest instance for MD5
+            MessageDigest md = MessageDigest.getInstance("MD5");
+
+            // Add password bytes to digest
+            md.update(password.getBytes());
+
+            // Get the hash's bytes
+            byte[] bytes = md.digest();
+
+            // These bytes[] has bytes in decimal format. Convert it to hexadecimal format
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < bytes.length; i++) {
+                sb.append(Integer.toString((bytes[i] & 0xff) + 0x100, 16).substring(1));
+            }
+
+            // Get complete hashed password in hex format
+            generatedPassword = sb.toString();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        return generatedPassword;
     }
 }
