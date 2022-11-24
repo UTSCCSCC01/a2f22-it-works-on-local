943074eb72f626cc4ea6fa33546258ee17f3b9f5
mojohosseinii <hosseini.sayedmojtaba@gmail.com>
2022-11-24 18:10:48 -0500

    [TripInfoMicroservice-Endpoints2-5] - Created Business logic for endpoints 2, 3, 4, 5
    2: POST /trip/confirm
    3: PATCH /trip/:_id
    4: GET /trip/passenger/:uid
    5: GET /trip/driver/:uid

--- TripinfoMicroservice/src/main/java/ca/utoronto/utm/mcs/Driver.java
+++ TripinfoMicroservice/src/main/java/ca/utoronto/utm/mcs/Driver.java
@@ -1,7 +1,12 @@
 package ca.utoronto.utm.mcs;
 
+import com.mongodb.client.FindIterable;
 import com.sun.net.httpserver.HttpExchange;
+import org.bson.Document;
+import org.json.JSONArray;
 import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.IOException;
 
 public class Driver extends Endpoint {
@@ -16,5 +21,36 @@ public class Driver extends Endpoint {
     @Override
     public void handleGet(HttpExchange r) throws IOException, JSONException {
         // TODO
+        String params[] = r.getRequestURI().toString().split("/");
+        if (params.length != 4 || params[3].isEmpty()) {
+            this.sendStatus(r, 400);
+            return;
+        }
+        String did = params[3];
+
+
+        try {
+            FindIterable<Document> result = this.dao.getTripByDriverId(did);
+            if (result.cursor().hasNext()) {
+                JSONObject res = new JSONObject();
+                JSONObject data = new JSONObject();
+                data.put("trips", findIterableToJSONArray(result));
+;                res.put("data", data);
+                this.sendResponse(r, res, 200);
+                return;
+            }
+            this.sendStatus(r, 404);
+        } catch (Exception e) {
+            this.sendStatus(r, 500);
+        }
+    }
+    public static JSONArray findIterableToJSONArray(FindIterable<Document> docs) throws Exception {
+        JSONArray arr = new JSONArray();
+        int i = 0;
+        for (Document doc : docs) {
+            arr.put(i, new JSONObject(doc.toJson()));
+            i++;
+        }
+        return arr;
     }
 }
