08cf81b1f5e4da8ffd4464ccb6cf85e5b1836774
Yuval Kamani <yuvalkamani@gmail.com>
2022-11-24 16:25:23 -0500

    [yuval/TripInfoMicroservice-request] - Implemented Request endpoint for TripInfo

--- TripinfoMicroservice/src/main/java/ca/utoronto/utm/mcs/MongoDao.java
+++ TripinfoMicroservice/src/main/java/ca/utoronto/utm/mcs/MongoDao.java
@@ -1,25 +1,29 @@
 package ca.utoronto.utm.mcs;
 
-import com.mongodb.client.MongoCollection;
+import com.mongodb.MongoClient;
+import com.mongodb.client.*;
 import com.mongodb.*;
-import com.mongodb.client.FindIterable;
+import io.github.cdimascio.dotenv.Dotenv;
 import org.bson.Document;
 import com.mongodb.client.MongoCollection;
-import com.mongodb.client.MongoDatabase;
 
 
 public class MongoDao {
 
 	public MongoCollection<Document> collection;
+	private final String username = "root";
+	private final String password = "123456";
 	MongoDatabase mongoDatabase;
 	MongoClient mongoClient;
 
 	public MongoDao() {
-        // TODO: 
-        // Connect to the mongodb database and create the database and collection. 
-        // Use Dotenv like in the DAOs of the other microservices.
-		MongoClientURI uri = new MongoClientURI("mongodb://root:123456@mongodb/?authSource=admin");
-		MongoClient mongoClient = new MongoClient(uri);
+		// TODO:
+		// Connect to the mongodb database and create the database and collection.
+		// Use Dotenv like in the DAOs of the other microservices.
+		Dotenv dotenv = Dotenv.load();
+		String addr = dotenv.get("MONGODB_ADDR");
+		String uri = String.format("mongodb://%s:%s@", username, password) +"localhost"+":27017";
+		this.mongoClient = new MongoClient("localhost", 27017);
 		this.mongoDatabase = mongoClient.getDatabase("trip");
 		this.collection =  mongoClient.getDatabase("trip").getCollection("trips");
 	}
