a54a13387aa777dc932300c2a4580a8981874fda
mojohosseinii <hosseini.sayedmojtaba@gmail.com>
2022-11-24 22:21:19 -0500

    [APIgateway] - Set up APIgateway

--- ApiGateway/src/main/java/ca/utoronto/utm/mcs/RequestRouter.java
+++ ApiGateway/src/main/java/ca/utoronto/utm/mcs/RequestRouter.java
@@ -3,11 +3,13 @@ package ca.utoronto.utm.mcs;
 import java.io.IOException;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
+import org.json.JSONException;
+import org.json.JSONObject;
 
-/** 
- * Everything you need in order to send and recieve httprequests to 
- * the microservices is given here. Do not use anything else to send 
- * and/or recieve http requests from other microservices. Any other 
+/**
+ * Everything you need in order to send and recieve httprequests to
+ * the microservices is given here. Do not use anything else to send
+ * and/or recieve http requests from other microservices. Any other
  * imports are fine.
  */
 import java.net.http.HttpClient;
@@ -15,19 +17,99 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.net.URI;
 import java.io.OutputStream;    // Also given to you to send back your response
+import java.util.HashMap;
 
 public class RequestRouter implements HttpHandler {
-	
+
     /**
-     * You may add and/or initialize attributes here if you 
+     * You may add and/or initialize attributes here if you
      * need.
      */
+	public HashMap<Integer, String> errorMap;
 	public RequestRouter() {
-
+		errorMap = new HashMap<>();
+		errorMap.put(200, "OK");
+		errorMap.put(400, "BAD REQUEST");
+		errorMap.put(401, "UNAUTHORIZED");
+		errorMap.put(404, "NOT FOUND");
+		errorMap.put(405, "METHOD NOT ALLOWED");
+		errorMap.put(409, "CONFLICT");
+		errorMap.put(500, "INTERNAL SERVER ERROR");
 	}
 
 	@Override
-	public void handle(HttpExchange r) throws IOException {
+	public void handle(HttpExchange r) throws IOException{
         // TODO
+		//r.getResponseHeaders().add("Access-Control-Allow-Origin", "*"); // For CORS
+		try {
+			URI uri = r.getRequestURI();
+			String p = uri.getPath();
+			String query = uri.getQuery();
+			String method = r.getRequestMethod();
+			String body = Utils.convert(r.getRequestBody());
+			String final_p="";
+
+			System.out.printf("uri %s\n", uri);
+
+			String uri_string = uri.toString();
+
+			if(uri_string.contains("location")){
+				if(query == null){
+					final_p = "http://locationmicroservice:8000" + p;
+				} else {
+					final_p = "http://locationmicroservice:8000" + p + "?" + query;
+				}
+ 			} else if (uri_string.contains("user")){
+				if(query == null){
+					final_p = "http://usermicroservice:8000" + p;
+				} else {
+					final_p = "http://usermicroservice:8000" + p + "?" + query;
+				}
+			} else if (uri_string.contains("trip")){
+				if(query == null){
+					final_p = "http://tripinfomicroservice:8000" + p;
+				} else {
+					final_p = "http://tripinfomicroservice:8000" + p + "?" + query;
+				}
+			} else {
+				this.sendStatus(r, 404);
+			}
+
+			System.out.printf("Final_p = %s", final_p);
+
+			HttpClient c1 = HttpClient.newBuilder().build();
+
+			HttpRequest r1 = HttpRequest.
+					newBuilder().
+					uri(URI.create(final_p)).
+					method(method, HttpRequest.BodyPublishers.ofString(body)).build();
+
+			HttpResponse<String> res = c1.send(r1, HttpResponse.BodyHandlers.ofString());
+			JSONObject obj = new JSONObject(res.body());
+			this.sendResponse(r, obj, res.statusCode());
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
+	public void sendResponse(HttpExchange r, JSONObject obj, int statusCode) throws JSONException, IOException {
+		obj.put("status", errorMap.get(statusCode));
+		String response = obj.toString();
+		r.sendResponseHeaders(statusCode, response.length());
+		this.writeOutputStream(r, response);
+	}
+	public void writeOutputStream(HttpExchange r, String response) throws IOException {
+		OutputStream os = r.getResponseBody();
+		os.write(response.getBytes());
+		os.close();
+	}
+
+	public void sendStatus(HttpExchange r, int statusCode) throws JSONException, IOException {
+		JSONObject res = new JSONObject();
+		res.put("status", errorMap.get(statusCode));
+		String response = res.toString();
+		r.sendResponseHeaders(statusCode, response.length());
+		this.writeOutputStream(r, response);
+	}
+
 }
--- locationmicroservice/src/test/java/ca/utoronto/utm/mcs/AppTest.java
+++ locationmicroservice/src/test/java/ca/utoronto/utm/mcs/AppTest.java
@@ -69,46 +69,46 @@ public class AppTest {
         JSONObject body1 = new JSONObject();
         body1.put("uid","john");
         body1.put("is_driver",true);
-        put_request("http://localhost:8000/location/user", body1);
+        put_request("http://localhost:8004/location/user", body1);
 
         JSONObject body2 = new JSONObject();
         body2.put("uid","ilir");
         body2.put("is_driver",false);
-        put_request("http://localhost:8000/location/user", body2);
+        put_request("http://localhost:8004/location/user", body2);
 
         JSONObject body3 = new JSONObject();
         body3.put("longitude",78.0732);
         body3.put("latitude",42.0543);
         body3.put("street","road1");
-        patch_request("http://localhost:8000/location/john", body3);
+        patch_request("http://localhost:8004/location/john", body3);
 
         JSONObject body4 = new JSONObject();
         body4.put("longitude",78.0753);
         body4.put("latitude",42.0532);
         body4.put("street","road2");
-        patch_request("http://localhost:8000/location/ilir", body4);
+        patch_request("http://localhost:8004/location/ilir", body4);
 
         JSONObject body5 = new JSONObject();
         body5.put("roadName","road1");
         body5.put("hasTraffic",true);
-        put_request("http://localhost:8000/location/road", body5);
+        put_request("http://localhost:8004/location/road", body5);
 
         JSONObject body6 = new JSONObject();
         body6.put("roadName","road2");
         body6.put("hasTraffic",true);
-        put_request("http://localhost:8000/location/road", body6);
+        put_request("http://localhost:8004/location/road", body6);
 
         JSONObject body7 = new JSONObject();
         body7.put("roadName1", "road1");
         body7.put("roadName2", "road2");
         body7.put("hasTraffic", true);
         body7.put("time", 7);
-        post_request("http://localhost:8000/location/hasRoute", body7);
+        post_request("http://localhost:8004/location/hasRoute", body7);
 
         HttpClient c = HttpClient.newBuilder().build();
         HttpRequest r = HttpRequest.
                 newBuilder().
-                uri(URI.create("http://localhost:8000/location/nearbyDriver/ilir?radius=2")).build();
+                uri(URI.create("http://localhost:8004/location/nearbyDriver/ilir?radius=2")).build();
         HttpResponse<String> res = c.send(r, HttpResponse.BodyHandlers.ofString());
 
         assertEquals(200, res.statusCode());
@@ -121,7 +121,7 @@ public class AppTest {
         HttpClient c = HttpClient.newBuilder().build();
         HttpRequest r = HttpRequest.
                 newBuilder().
-                uri(URI.create("http://localhost:8000/location/nearbyDriver/john?radius=-1")).build();
+                uri(URI.create("http://localhost:8004/location/nearbyDriver/john?radius=-1")).build();
         HttpResponse<String> res = c.send(r, HttpResponse.BodyHandlers.ofString());
 
         assertEquals(404, res.statusCode());
@@ -133,7 +133,7 @@ public class AppTest {
         HttpClient c = HttpClient.newBuilder().build();
         HttpRequest r = HttpRequest.
                 newBuilder().
-                uri(URI.create("http://localhost:8000/location/navigation/john?passengerUid=ilir")).build();
+                uri(URI.create("http://localhost:8004/location/navigation/john?passengerUid=ilir")).build();
         HttpResponse<String> res = c.send(r, HttpResponse.BodyHandlers.ofString());
 
         assertEquals(200, res.statusCode());
@@ -146,7 +146,7 @@ public class AppTest {
         HttpClient c = HttpClient.newBuilder().build();
         HttpRequest r = HttpRequest.
                 newBuilder().
-                uri(URI.create("http://localhost:8000/location/navigation/john?passengerUid=ilir12")).build();
+                uri(URI.create("http://localhost:8004/location/navigation/john?passengerUid=ilir12")).build();
         HttpResponse<String> res = c.send(r, HttpResponse.BodyHandlers.ofString());
 
         assertEquals(400, res.statusCode());
